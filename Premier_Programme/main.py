5fb38ffeb4756d93b789d15d82b67eaa8c548439
Elisee-des <esabidani@gmail.com>
2023-05-26 02:18:09 +0000

    Gestion de erreurs

--- Premier_Programme/main.py
+++ Premier_Programme/main.py
@@ -1,10 +1,14 @@
 
 nom = input("Quel est ton nom ? ")
 age = input("Quel est votre age ? ")
-try:
-    age_prochain = int(age) + 1
-except:
-    print("ERREUR: Vous devez entrez un chiffre.")
-else:
-    print("Vous vous appelz " + nom + ", votre age est " + str(age) + ".")
-    print("L'annee prochain vous aurez " + age_prochain + "ans")
+
+age = 0
+while age == 0:
+    age_str = input("Quel est votre age ? ")
+    try:
+        age = int(age_str)
+    except:
+        print("ERREUR: Vous devez entrez un chiffre.")
+
+print("Vous vous appelz " + nom + ", votre age est " + str(age) + ".")
+print("L'annee prochain vous aurez " + str(age+1) + " ans")
