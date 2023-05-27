76ea7f2557e11c84a605f92042f8dcd51f124fb6
Elisee-des <esabidani@gmail.com>
2023-05-27 00:16:12 +0000

    Exercice fonction avc parametre

--- Premier_Programme/main_3.py
+++ Premier_Programme/main_3.py
@@ -6,10 +6,10 @@ def demander_nom():
     return nom
 
 
-def demander_age():
+def demander_age(nom_personne):
     age = 0
     while age == 0:
-        age_str = input("Quel est votre age ? ")
+        age_str = input(nom_personne + ", Quel est votre age ? ")
         try:
             age = int(age_str)
         except:
@@ -18,8 +18,10 @@ def demander_age():
 
 
 nom_utlisateur = demander_nom()
+nom_utlisateur2 = demander_nom()
 
-age_demander = demander_age()
+age_demander = demander_age(nom_utlisateur)
+age_demander2 = demander_age(nom_utlisateur2)
 
 
 print("Vous vous appelz " + nom_utlisateur + ", votre age est " + str(age_demander) + ".")
